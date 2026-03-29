827c30e241777851235506975d5ce780ea38d382
Punyapal Shah <mrpunyapal@gmail.com>
2026-03-29 22:09:48 +0530

    chore: scaffold project structure with tooling configs

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorPest\Set\PestLevelSetList;
+use RectorPest\Set\PestSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/config',
+        __DIR__.'/tests',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+    )
+    ->withSets([
+        PestSetList::PEST_CODE_QUALITY,
+        PestLevelSetList::UP_TO_PEST_30,
+    ])
+    ->withPhpSets()
+    ->withImportNames();
